ba60564036b2cbda0424e181a0bf304ca74e90ab
Alexander Vostres <alex.vostres@gmail.com>
2018-09-03 20:11:45 +0300

    Fix case when destination file is one of source files

--- ILRepack/ILRepack.cs
+++ ILRepack/ILRepack.cs
@@ -262,6 +262,9 @@ namespace ILRepacking
             timer.Start();
             Options.Validate();
             PrintRepackHeader();
+
+            var actualOutFile = Options.OutputFile;
+            Options.OutputFile = GetTempFile(Options.OutputFile);
             _reflectionHelper = new ReflectionHelper(this);
             ResolveSearchDirectories();
 
@@ -359,15 +362,18 @@ namespace ILRepacking
                 
                 sourceServerDataStep.Write();
 
-                for (int i = 1; i < MergedAssemblies.Count; ++i)
+                foreach (var assembly in MergedAssemblies)
                 {
-                    MergedAssemblies[i].Dispose();
+                    assembly.Dispose();
                 }
                 
                 TargetAssemblyDefinition.Dispose();
                 GlobalAssemblyResolver.Dispose();
 
                 win32ResourceStep.Patch(Options.OutputFile);
+
+                MoveTempFile(Options.OutputFile, actualOutFile);
+                Options.OutputFile = actualOutFile;
 
                 // If this is an executable and we are on linux/osx we should copy file permissions from
                 // the primary assembly
@@ -388,6 +394,39 @@ namespace ILRepacking
             }
 
             Logger.Info($"Finished in {timer.Elapsed}");
+        }
+
+        private void MoveTempFile(string tempFile, string outFile)
+        {
+            var srcDir = Path.GetDirectoryName(tempFile);
+            var tgtDir = Path.GetDirectoryName(outFile);
+
+            foreach (var srcFileName in Directory.EnumerateFiles(srcDir))
+            {
+                var fileName = Path.GetFileName(srcFileName);
+                var tgtFileName = GetFileName(tgtDir, fileName);
+                if (File.Exists(tgtFileName))
+                {
+                    File.Delete(tgtFileName);
+                }
+                File.Move(srcFileName, tgtFileName);
+            }
+
+            Directory.Delete(srcDir, false);
+        }
+
+        private string GetFileName(string tgtDir, string fileName)
+        {
+            if (string.IsNullOrEmpty(tgtDir)) return fileName;
+            return $"{tgtDir}\\{fileName}";
+        }
+
+        private string GetTempFile(string outputFileName)
+        {
+            var fileName = Path.GetFileName(outputFileName);
+            var dirName = $"{Path.GetTempPath()}{Path.GetRandomFileName()}";
+            Directory.CreateDirectory(dirName);
+            return $"{dirName}\\{fileName}";
         }
 
         private ISourceServerDataRepackStep GetSourceServerDataStep(bool isUnixEnvironment)
